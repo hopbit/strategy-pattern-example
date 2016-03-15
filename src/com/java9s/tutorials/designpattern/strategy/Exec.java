3e63c95ea846a6ebc6c47293f5c9688a74fa1640
java9s <mail@java9s.com>
2016-03-15 11:47:42 +0100

    Added the Navigation code

--- src/com/java9s/tutorials/designpattern/strategy/Exec.java
+++ src/com/java9s/tutorials/designpattern/strategy/Exec.java
@@ -2,6 +2,7 @@ package com.java9s.tutorials.designpattern.strategy;
 
 public class Exec {
 	public static void main(String[] args) {
-
+		Navigation navigation = new Navigation();
+		navigation.navigate();
 	}
 }
